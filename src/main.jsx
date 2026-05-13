2bce34c89f7d8f8bfb4c6efcf41eaadbe1ca442a
aksaecx <aksafigma@gmail.com>
2026-05-13 21:29:46 +0800

    Fi

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
+import './i18n'; // Tambahkan baris ini
 
-// === BARIS AJAIB: MENGHIDUPKAN MESIN PWA ===
-import { registerSW } from 'virtual:pwa-register'
-
-// Langsung eksekusi fungsinya, JANGAN pakai const updateSW =
-registerSW({
-  onNeedRefresh() {
-    console.log("PWA Update Ready!");
-  },
-  onOfflineReady() {
-    console.log("PWA Ready to work offline!");
-  },
-});
-// ===========================================
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
